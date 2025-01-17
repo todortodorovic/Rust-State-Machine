ec180f08454cb2f5ce36a46ed2b449688fd5b6df
todortodorovic <95745674+todortodorovic@users.noreply.github.com>
2025-01-17 11:04:09 +0100

    proof of existence

--- src/balances.rs
+++ src/balances.rs
@@ -1,49 +1,49 @@
+use num::traits::{CheckedAdd, CheckedSub, Zero};
 use std::collections::BTreeMap;
-use num::traits::{CheckedAdd,CheckedSub,Zero};
 
-pub trait Config: crate::system::Config{
-   
-   type Balance:Zero+CheckedSub+CheckedAdd+Copy;
+pub trait Config: crate::system::Config {
+	type Balance: Zero + CheckedSub + CheckedAdd + Copy;
 }
-
 
 #[derive(Debug)]
-pub struct Pallet<T:Config>{
-    balances:BTreeMap<T::AccountId,T::Balance>,
+pub struct Pallet<T: Config> {
+	balances: BTreeMap<T::AccountId, T::Balance>,
 }
 
-impl<T:Config> Pallet<T> 
-{
-    pub fn new()->Self{
-        Self { balances: BTreeMap::new()
-         }
-    }
+impl<T: Config> Pallet<T> {
+	pub fn new() -> Self {
+		Self { balances: BTreeMap::new() }
+	}
 
-    pub fn set_balance(&mut self,who:&T::AccountId, amount:T::Balance){
-        self.balances.insert(who.clone(),amount);
-    }
+	pub fn set_balance(&mut self, who: &T::AccountId, amount: T::Balance) {
+		self.balances.insert(who.clone(), amount);
+	}
 
-    pub fn balance(&self,who:&T::AccountId)->T::Balance{
-        *self.balances.get(who).unwrap_or(&T::Balance::zero())
-    }
+	pub fn balance(&self, who: &T::AccountId) -> T::Balance {
+		*self.balances.get(who).unwrap_or(&T::Balance::zero())
+	}
 
-    pub fn transfer(&mut self,caller:T::AccountId, to:T::AccountId,amount:T::Balance)->crate::support::DispatchResult{
-        let caller_balance=self.balance(&caller);
-        let to_balance=self.balance(&to);
+	pub fn transfer(
+		&mut self,
+		caller: T::AccountId,
+		to: T::AccountId,
+		amount: T::Balance,
+	) -> crate::support::DispatchResult {
+		let caller_balance = self.balance(&caller);
+		let to_balance = self.balance(&to);
 
-        let new_caller_balance=caller_balance.checked_sub(&amount).ok_or("Not enough funds.")?;
-        let new_to_balance=to_balance.checked_add(&amount).ok_or("Overflow")?;
+		let new_caller_balance = caller_balance.checked_sub(&amount).ok_or("Not enough funds.")?;
+		let new_to_balance = to_balance.checked_add(&amount).ok_or("Overflow")?;
 
-        self.balances.insert(caller, new_caller_balance);
-        self.balances.insert(to, new_to_balance);
-     
-       Ok(())
-    }
+		self.balances.insert(caller, new_caller_balance);
+		self.balances.insert(to, new_to_balance);
 
+		Ok(())
+	}
 }
 
 pub enum Call<T: Config> {
-	Transfer{to: T::AccountId,amount:T::Balance},
+	Transfer { to: T::AccountId, amount: T::Balance },
 }
 
 /// Implementation of the dispatch logic, mapping from `BalancesCall` to the appropriate underlying
@@ -58,20 +58,18 @@ impl<T: Config> crate::support::Dispatch for Pallet<T> {
 		call: Self::Call,
 	) -> crate::support::DispatchResult {
 		match call {
-            Call::Transfer { to, amount }=>{
-                self.transfer(caller, to, amount)?;
-            },
-            
-        }
-        Ok(())
+			Call::Transfer { to, amount } => {
+				self.transfer(caller, to, amount)?;
+			},
+		}
+		Ok(())
 	}
 }
 
-
 #[cfg(test)]
-mod tests{
-    struct TestConfig;
-    impl crate::system::Config for TestConfig {
+mod tests {
+	struct TestConfig;
+	impl crate::system::Config for TestConfig {
 		type AccountId = String;
 		type BlockNumber = u32;
 		type Nonce = u32;
@@ -79,22 +77,22 @@ mod tests{
 	impl super::Config for TestConfig {
 		type Balance = u128;
 	}
-    #[test]
-    fn init_balances(){
-        let mut balances=super::Pallet::<TestConfig>::new();
-        assert_eq!(balances.balance(&"alice".to_string()),0);
-        balances.set_balance(&"alice".to_string(), 100);
-	    assert_eq!(balances.balance(&"alice".to_string()), 100);
-	    assert_eq!(balances.balance(&"bob".to_string()), 0);
-    }
-    #[test]
-    fn tranfer_balance(){
-        let mut balances=super::Pallet::<TestConfig>::new();
-        assert_eq!(
+	#[test]
+	fn init_balances() {
+		let mut balances = super::Pallet::<TestConfig>::new();
+		assert_eq!(balances.balance(&"alice".to_string()), 0);
+		balances.set_balance(&"alice".to_string(), 100);
+		assert_eq!(balances.balance(&"alice".to_string()), 100);
+		assert_eq!(balances.balance(&"bob".to_string()), 0);
+	}
+	#[test]
+	fn tranfer_balance() {
+		let mut balances = super::Pallet::<TestConfig>::new();
+		assert_eq!(
 			balances.transfer("alice".to_string(), "bob".to_string(), 51),
 			Err("Not enough funds.")
 		);
-        balances.set_balance(&"alice".to_string(), 100);
+		balances.set_balance(&"alice".to_string(), 100);
 		assert_eq!(balances.transfer("alice".to_string(), "bob".to_string(), 51), Ok(()));
 		assert_eq!(balances.balance(&"alice".to_string()), 49);
 		assert_eq!(balances.balance(&"bob".to_string()), 51);
@@ -103,6 +101,5 @@ mod tests{
 			balances.transfer("alice".to_string(), "bob".to_string(), 51),
 			Err("Not enough funds.")
 		);
-
-    }
+	}
 }
--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,60 @@
 mod balances;
-mod system;
+mod proof_of_existence;
 mod support;
+mod system;
 use crate::support::Dispatch;
 
-mod types{
-    pub type AccountId=String;
-    pub type Balance=u128;
-    pub type BlockNumber=u32;
-    pub type Nonce=u32;
-    pub type Extrinsic=crate::support::Extrinsic<AccountId,crate::RuntimeCall>;
-    pub type Header=crate::support::Header<BlockNumber>;
-    pub type Block=crate::support::Block<Header,Extrinsic>;
+mod types {
+	pub type AccountId = String;
+	pub type Balance = u128;
+	pub type BlockNumber = u32;
+	pub type Nonce = u32;
+	pub type Extrinsic = crate::support::Extrinsic<AccountId, crate::RuntimeCall>;
+	pub type Header = crate::support::Header<BlockNumber>;
+	pub type Block = crate::support::Block<Header, Extrinsic>;
+	pub type Content = &'static str;
 }
 
-pub enum RuntimeCall{
-    Balances(balances::Call<Runtime>),
- }
+pub enum RuntimeCall {
+	Balances(balances::Call<Runtime>),
+	ProofOfExistence(proof_of_existence::Call<Runtime>),
+}
 
 #[derive(Debug)]
-pub struct Runtime{
-    system:system::Pallet<Self>,
-    balances:balances::Pallet<Self>
+pub struct Runtime {
+	system: system::Pallet<Self>,
+	balances: balances::Pallet<Self>,
+	proof_of_existance: proof_of_existence::Pallet<Self>,
 }
-impl system::Config for Runtime{
-    type AccountId=types::AccountId;
-    type BlockNumber=types::BlockNumber;
-    type Nonce=types::Nonce;
+impl system::Config for Runtime {
+	type AccountId = types::AccountId;
+	type BlockNumber = types::BlockNumber;
+	type Nonce = types::Nonce;
 }
 
-impl balances::Config for Runtime{
-    type Balance=types::Balance;
+impl balances::Config for Runtime {
+	type Balance = types::Balance;
 }
-impl Runtime{
-    fn new()->Self{
-        Self{system:system::Pallet::new(),balances:balances::Pallet::new()}
-    }
 
-    fn execute_block(&mut self, block: types::Block)->support::DispatchResult{
-        self.system.inc_block_number();
-		if block.header.block_number != self.system.block_number() {
-			return Err("block number does not match what is expected")
+impl proof_of_existence::Config for Runtime {
+	type Content = types::Content;
+}
+
+impl Runtime {
+	fn new() -> Self {
+		Self {
+			system: system::Pallet::new(),
+			balances: balances::Pallet::new(),
+			proof_of_existance: proof_of_existence::Pallet::new(),
 		}
-		
-		
+	}
+
+	fn execute_block(&mut self, block: types::Block) -> support::DispatchResult {
+		self.system.inc_block_number();
+		if block.header.block_number != self.system.block_number() {
+			return Err("block number does not match what is expected");
+		}
+
 		for (i, support::Extrinsic { caller, call }) in block.extrinsics.into_iter().enumerate() {
 			self.system.inc_nonce(&caller);
 			let _res = self.dispatch(caller, call).map_err(|e| {
@@ -52,10 +64,9 @@ impl Runtime{
 				)
 			});
 		}
-        Ok(())
-    }
+		Ok(())
+	}
 }
-
 
 impl crate::support::Dispatch for Runtime {
 	type Caller = <Runtime as system::Config>::AccountId;
@@ -66,36 +77,90 @@ impl crate::support::Dispatch for Runtime {
 		caller: Self::Caller,
 		runtime_call: Self::Call,
 	) -> support::DispatchResult {
-		match runtime_call{
-            RuntimeCall::Balances(call) =>{
-                self.balances.dispatch(caller,call)?},
-        }
-        Ok(())
+		match runtime_call {
+			RuntimeCall::Balances(call) => self.balances.dispatch(caller, call)?,
+
+			RuntimeCall::ProofOfExistence(call) => {
+				self.proof_of_existance.dispatch(caller, call)?;
+			},
+		}
+		Ok(())
 	}
 }
 
 
 fn main() {
-	let mut runtime=Runtime::new();
-    let alice="alice".to_string();
-    let bob="bob".to_string();
-    let charlie="charlie".to_string();
-    runtime.balances.set_balance(&alice, 100);
-    
-    let block_1 = types::Block {
+	
+	
+	let mut runtime = Runtime::new();
+	let alice = "alice".to_string();
+	let bob = "bob".to_string();
+	let charlie = "charlie".to_string();
+
+	
+	runtime.balances.set_balance(&alice, 100);
+
+	
+	
+	let block_1 = types::Block {
 		header: support::Header { block_number: 1 },
 		extrinsics: vec![
 			support::Extrinsic {
 				caller: alice.clone(),
-				call: RuntimeCall::Balances(balances::Call::Transfer { to: bob, amount: 30 }),
+				call: RuntimeCall::Balances(balances::Call::Transfer {
+					to: bob.clone(),
+					amount: 30,
+				}),
 			},
 			support::Extrinsic {
-				caller: alice,
+				caller: alice.clone(),
 				call: RuntimeCall::Balances(balances::Call::Transfer { to: charlie, amount: 20 }),
 			},
 		],
 	};
-    runtime.execute_block(block_1).expect("inbalid block");
-    println!("{:#?}",runtime);
 
+	let block_2 = types::Block {
+		header: support::Header { block_number: 2 },
+		extrinsics: vec![
+			support::Extrinsic {
+				caller: alice.clone(),
+				call: RuntimeCall::ProofOfExistence(proof_of_existence::Call::CreateClaim {
+					claim: "Hello, world!",
+				}),
+			},
+			support::Extrinsic {
+				caller: bob.clone(),
+				call: RuntimeCall::ProofOfExistence(proof_of_existence::Call::CreateClaim {
+					claim: "Hello, world!",
+				}),
+			},
+		],
+	};
+
+	let block_3 = types::Block {
+		header: support::Header { block_number: 3 },
+		extrinsics: vec![
+			support::Extrinsic {
+				caller: alice,
+				call: RuntimeCall::ProofOfExistence(proof_of_existence::Call::RevokeClaim {
+					claim: "Hello, world!",
+				}),
+			},
+			support::Extrinsic {
+				caller: bob,
+				call: RuntimeCall::ProofOfExistence(proof_of_existence::Call::CreateClaim {
+					claim: "Hello, world!",
+				}),
+			},
+		],
+	};
+
+	
+	
+	runtime.execute_block(block_1).expect("invalid block");
+	runtime.execute_block(block_2).expect("invalid block");
+	runtime.execute_block(block_3).expect("invalid block");
+
+	
+	println!("{:#?}", runtime);
 }
